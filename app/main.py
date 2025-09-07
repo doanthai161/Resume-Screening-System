201f12a84b119b0e892fd2e0bc48e77532de7d13
thaidoan161 <thaidoan16103@gmail.com>
2025-09-07 09:45:25 +0700

    setup main & env

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.repositories.database import init_db
+import uvicorn
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+async def hello():
+    return {"msg": "Resume Screening API is running!"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
