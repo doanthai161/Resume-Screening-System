df48ac3b7bee8b4b704013358287cfa00ef4c721
mediax <mediax@MacBook-Air-cua-mediax.local>
2025-12-22 15:08:51 +0700

    set up permission nam collection

--- app/models/permission.py
+++ app/models/permission.py
@@ -1,6 +1,7 @@
 from pydantic import Field
 from beanie import Document
 from datetime import datetime, timezone
+from pymongo import IndexModel
 
 
 class Permission(Document):
@@ -8,3 +9,11 @@ class Permission(Document):
     description: str = Field(..., description="Description of the permission")
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+    class Settings:
+        name = "permissions"
+        indexes = [
+            IndexModel([("name", 1)], name="idx_permissions_name", unique=True),
+        ]
+    class Config:
+        arbitrary_types_allowed = True
